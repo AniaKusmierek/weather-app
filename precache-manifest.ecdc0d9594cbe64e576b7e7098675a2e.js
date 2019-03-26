c6988948c1ace13c22151a070e78a147454349f9
Anna Kuśmierek <aniak1100@gmail.com>
2019-03-26 12:19:23 +0000

    Updates

---
+++ precache-manifest.ecdc0d9594cbe64e576b7e7098675a2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "3c5bdd3802381f5a7bb1",
+    "url": "/weather-app/static/js/main.3c5bdd38.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/weather-app/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "3c5bdd3802381f5a7bb1",
+    "url": "/weather-app/static/css/main.df4f09e4.chunk.css"
+  },
+  {
+    "revision": "fd3d6ab1a165f95c2f0b12ea44d97136",
+    "url": "/weather-app/index.html"
+  }
+];
